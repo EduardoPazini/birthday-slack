efa2ded8f742c61519a382e9f4f2d400f1eab4f3
Murilo <murilo.on@hotmail.com>
2018-08-29 15:47:35 -0300

    Maybe fixed

--- app.py
+++ app.py
@@ -36,10 +36,10 @@ def isSomeoneBirthday():
     return "Ninguém faz aniversário hoje :/"
 
 
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=8)
-# def scheduled_job():
-#     print('This job is run every weekday at 5pm.')
-#     isSomeoneBirthday()
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
+    isSomeoneBirthday()
 
 
 @sched.scheduled_job('interval', minutes=1)
