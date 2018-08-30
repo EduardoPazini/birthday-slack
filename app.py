afbdbfd952cb2b14473e1faf0ae93571e6b3e086
Murilo <murilo.on@hotmail.com>
2018-08-30 13:51:31 -0300

    dotenv

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
 from datetime import datetime
 from requests import post
-#import birthday_register
+
+# import birthday_register
+from dotenv import load_dotenv
+import os
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-sched = BlockingScheduler()
+load_dotenv()
 
-#SLACK_URL = "https://slack.com/api/chat.postMessage"
-HEADER = {
-        "Authorization": ""
-}
-BIRTHS = {
-    "Murilo": "8/15/1999",
-    "Carlos": "8/29/2018",
-    "Daniel": "2/2/2018"
-}
+HEADER = {"Authorization": os.getenv("TOKEN")}
+BIRTHS = {"Murilo": "8/15/1999", "Carlos": "8/29/2018", "Daniel": "2/2/2018"}
 
 
 def formatMessage(nome):
     message = {
-        "channel": "",
-        "text": "FELIZ ANIVERSÁRIO, {}!!!".format(nome)
+        "channel": os.getenv("CHANNEL"),
+        "text": "FELIZ ANIVERSÁRIO, {}!!!".format(nome),
     }
 
     return message
@@ -32,20 +28,24 @@ def isSomeoneBirthday():
 
     for data in BIRTHS.items():
         if today == data[1]:
-            post(SLACK_URL, json=formatMessage(data[0]), headers=HEADER)
-    
+            post(os.getenv("SLACK_URL"), json=formatMessage(data[0]), headers=HEADER)
+
     return "Ninguém faz aniversário hoje :/"
 
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=19)
+sched = BlockingScheduler()
+
+
+@sched.scheduled_job("cron", day_of_week="mon-fri", hour=19)
 def scheduled_job():
-    print('This job is run every weekday at 19pm.')
+    print("This job is run every weekday at 19pm.")
     isSomeoneBirthday()
 
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job("interval", minutes=1)
 def timed_job():
-    print('This job is run every one minute.')
+    print("This job is run every one minute.")
     isSomeoneBirthday()
+
 
 sched.start()
